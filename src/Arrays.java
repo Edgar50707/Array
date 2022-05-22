f9e8983e0712691b8f84dd14749a29caf6577df9
Edgar50707 <105382844+Edgar50707@users.noreply.github.com>
2022-05-23 02:50:09 +0400

    Initial commit

---
+++ src/Arrays.java
@@ -0,0 +1,218 @@
+public class Arrays {
+    public static void main(String[] args) {
+         /*
+        int[] array = {360, 11, 105, 36, 89};
+        int max = array[0];
+        int min = array[0];
+        int minIndex = 0;
+        int maxIndex = 0;
+
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+                maxIndex = i;
+            }
+            if (array[i] < min) {
+                min = array[i];
+                minIndex = i;
+            }
+        }
+        System.out.println(maxIndex + "   " + minIndex);
+        System.out.println(max + " " + min);
+
+
+
+        int[] myArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};  //revers
+
+        for (int i = 0; i < myArray.length / 2; i++) {
+            int x = myArray[i];
+            myArray[i] = myArray[myArray.length - 1 - i];
+            myArray[myArray.length - 1 - i] = x;
+        }
+        for (int i = 0; i < myArray.length; i++) {
+            System.out.print(myArray[i] + " ");
+
+        }
+
+        int[] array1 = {4, -8, -9, 5, 3, 7, -7}; //220
+        int d = 0;
+        int b = 0;
+        for (int i = 0; i < array1.length; i++) {
+            if (array1[i] < 0) {
+                b++;
+            } else {
+                d++;
+            }
+        }
+        System.out.println("Drakan " + d);
+        System.out.println("Bacasakan " + b);
+
+
+        int[] array2 = {25, 89, 2, 2, 2};       //231
+        int mult = 0;
+        int sum = 0;
+        for (int i : array2) {
+            if (i % 2 == 0) {
+                mult = i * i;
+                sum += mult;
+
+            }
+        }
+        System.out.println(sum);
+
+
+
+        int[] array2 = {25, 89, 74, 12, 36, 53};     //232
+        int count = 0;
+        for (int i = 0; i < array2.length; i++) {
+            if (array2[i] % 2 == 0) {
+                count++;
+
+            }
+        }
+        System.out.println(count);
+
+
+
+        int[] array3 = {47, 21, 46, 32, 98, 14};   //233
+        int multiply = 1;
+        int addition = 0;
+        for (int num : array3) {
+            if (num % 2 == 0) {
+                multiply *= num;
+                addition += num;
+            }
+        }
+        System.out.println(multiply);
+        System.out.println(addition);
+
+
+
+        int[] array7 = {25, 46, 36, 3, 2,7,3};  //234
+        int x = 0;
+        int j = 0;
+        for (int i = 0; i < array7.length; i++) {
+            if (array7[i] % 2 != 0) {
+                x += array7[i];
+                j++;
+            }
+        }
+        System.out.println(x/j);
+
+
+        int[] array4 = {25, 46, 36, 3, 2};    //236
+        int count = 0;
+        int multiply = 1;
+        for (int num : array4) {
+            if (num % 2 != 0) {
+                multiply *= num;
+                count++;
+            }
+
+        }
+        System.out.println(count + " " + multiply);
+
+
+        int[] array5 = {0, 58, 65, 32, 0, 47};  //237
+        int count1 = 0;
+        for (int num : array5) {
+            if (num == 0) {
+                count1++;
+            }
+        }
+        System.out.println(count);
+
+
+        int[] array6 = {0, 27, 5, 32, 0, 4};  //244
+        int number = 1;
+        for (int num : array6) {
+            if (num % 5 == 2) {
+                number *= num;
+
+            }
+
+        }
+        System.out.println(number);
+
+
+        int[] x = {14, 78, 36, 47, 36};  //269
+        int[] y = {14, 78, 36, 47, 36};
+        int xSum = 0;
+        int ySum = 0;
+        for (int num : x) {
+            if (num % 2 == 0) {
+                xSum += num;
+            }
+        }
+        for (int num : y) {
+            if (num % 2 != 0) {
+                ySum += num;
+            }
+        }
+        System.out.println(xSum+ySum);
+
+
+        int[] xArr = {2, 2, 2,4};             //270
+        int[] yArr = {2, 4, 2, 2};
+        int xSum = 0;
+        int ySum = 0;
+        for (int n : xArr) {
+            xSum += n * n;
+        }
+        for (int n : yArr) {
+            ySum += n * n;
+        }
+        System.out.println(xSum+ySum);
+
+         int[] arr = {24, 78, 96, 32, 45, 96, 12,96}; //257
+        int max = arr[0];
+        int index = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+                index = i;
+            }
+        }
+        System.out.println(index);
+
+
+        int[] arr = {24, 78, 96, 32, 45, 96, 12,96}; //258
+        int max = arr[0];
+        int index = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] >= max) {
+                max = arr[i];
+                index = i;
+            }
+
+        }
+        System.out.println(index);
+
+
+        int[] arr = {24, 12, 96, 32, 45, 96, 12, 96}; //259
+        int min = arr[0];
+        int index = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+                index = i;
+            }
+        }
+        System.out.println(index);
+
+
+        int[] arr = {24, 12, 96, 32, 45, 96, 12, 96}; //260
+        int min = arr[0];
+        int index = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] <= min) {
+                min = arr[i];
+                index = i;
+            }
+        }
+        System.out.println(index);
+
+         */
+
+    }
+}
